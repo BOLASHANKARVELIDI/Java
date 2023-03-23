fdd758883c0def6ff4880051385cf8ac93eedca1
BOLA SHANKAR VELIDI <110627239+BOLASHANKARVELIDI@users.noreply.github.com>
2023-03-23 22:32:54 +0530

    Create Even_Odd.java

---
+++ Even_Odd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+ {
+      public static void main(String[] args) {
+          Scanner Sc=new Scanner(sytem.in);
+          int num=Sc.nextInt();
+
+
+     //checking whether the number is even or odd
+     if (num % 2 == 0)
+              System.out.println(num + " is Even");
+     else
+              System.out.println(num + " is odd");
+      }
+ }
